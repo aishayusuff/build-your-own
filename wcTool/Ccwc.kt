2ac3da1a83feb62328e371f65275dbaedf75ae55
aishayusuff <aishatyusuffe@gmail.com>
2024-06-23 16:10:37 +0200

    add default case if args.size = 1

--- wcTool/Ccwc.kt
+++ wcTool/Ccwc.kt
@@ -4,31 +4,34 @@ import java.nio.charset.Charset
 fun main (args: Array<String>) {
 
     try {
+        //Checks that args exist
+        if (args.isEmpty()) {
+            println("This program requires at least one argument!")
+        }
+
+        //checks length of args to use apt index
+        val file = File(args[(if (args.size > 1) 1 else 0)])
+
         //checks that file exists and is valid
-        val file = File(args[1]);
         if (!file.exists() && !file.isFile()) {
             println("This program requires a valid file path")
         }
 
-        var numOfBytesInFile = 0L
-        var numOfLines = 0
-        var wordCount = 0
-
         when (args[0]) {
             //command that outputs bytes of a file
             "-c" -> {
-                 numOfBytesInFile = file.length()
+                 val numOfBytesInFile = file.length()
                 println("This file ($file) has $numOfBytesInFile bytes")
             }
             //command that outputs num of lines in a file
             "-l" -> {
-                numOfLines = file.readLines().size
+                val numOfLines = file.readLines().size
                 println("This file $file contains $numOfLines lines");
             }
             //command that outputs num of words in a file
             "-w" -> {
                 val words = (file.readText()).split(Regex("\\s+"))
-                wordCount = words.count { it.isNotBlank() }
+                val wordCount = words.count { it.isNotBlank() }
                 println("This file $file contains $wordCount words")
             }
             //command that outputs the number of characters in a file.
@@ -36,9 +39,14 @@ fun main (args: Array<String>) {
                 val numOfChars = file.readText(Charset.forName("UTF-8")).length
                 println("This file $file contains $numOfChars numbers of characters")
             }
-            else -> {
-                println("Please enter a valid command")
+            //when no command is given
+            file.toString() -> {
+                println("This file $file contains ${file.readLines().size} lines and ${
+                    file.readText().split(Regex("\\s+")).count { it.isNotBlank() }
+                } words and has ${file.length()} bytes ")
             }
+            else -> {
+                println("Please enter a valid command")            }
         }
     } catch (e: Exception) {
         println("${e.message}")
