f5a775aaca1b6e7afadf1edc0be9ece34aff13ae
aishayusuff <aishatyusuffe@gmail.com>
2024-06-23 16:38:49 +0200

    refactors code for DRY

--- wcTool/Ccwc.kt
+++ wcTool/Ccwc.kt
@@ -2,51 +2,65 @@ import java.io.File
 import java.nio.charset.Charset
 
 fun main (args: Array<String>) {
+    //helper functions
+    fun getNumOfBytes(file: File): Long {
+        return file.length()
+    }
+
+    fun getNumOfLines(file: File): Int {
+        return file.readLines().size
+    }
+
+    fun getNumOfWords(file: File): Int {
+        return file.readText().split(Regex("\\s+")).count{it.isNotBlank()}
+    }
+
+    fun getNumOfChars(file: File): Int {
+        return file.readText(Charset.forName("UTF-8")).length
+    }
 
     try {
         //Checks that args exist
         if (args.isEmpty()) {
             println("This program requires at least one argument!")
+            return
         }
 
         //checks length of args to use apt index
         val file = File(args[(if (args.size > 1) 1 else 0)])
 
         //checks that file exists and is valid
-        if (!file.exists() && !file.isFile()) {
+        if (!file.exists() || !file.isFile()) {
             println("This program requires a valid file path")
+            return
         }
 
         when (args[0]) {
             //command that outputs bytes of a file
             "-c" -> {
-                 val numOfBytesInFile = file.length()
-                println("This file ($file) has $numOfBytesInFile bytes")
+                println("This file ($file) has ${getNumOfBytes(file)} bytes")
             }
             //command that outputs num of lines in a file
             "-l" -> {
-                val numOfLines = file.readLines().size
-                println("This file $file contains $numOfLines lines");
+                println("This file ($file) contains ${getNumOfLines(file)} lines")
             }
             //command that outputs num of words in a file
             "-w" -> {
-                val words = (file.readText()).split(Regex("\\s+"))
-                val wordCount = words.count { it.isNotBlank() }
-                println("This file $file contains $wordCount words")
+                println("This file ($file) contains ${getNumOfWords(file)} words")
             }
             //command that outputs the number of characters in a file.
             "-m" -> {
-                val numOfChars = file.readText(Charset.forName("UTF-8")).length
-                println("This file $file contains $numOfChars numbers of characters")
-            }
-            //when no command is given
-            file.toString() -> {
-                println("This file $file contains ${file.readLines().size} lines and ${
-                    file.readText().split(Regex("\\s+")).count { it.isNotBlank() }
-                } words and has ${file.length()} bytes ")
+                println("This file ($file) contains ${getNumOfChars(file)} numbers of characters")
             }
             else -> {
-                println("Please enter a valid command")            }
+                println(
+                    "This file ($file) contains ${getNumOfLines(file)} lines and ${getNumOfWords(file)} words and has ${
+                        getNumOfBytes(
+                            file
+                        )
+                    } bytes "
+                )
+            }
         }
     } catch (e: Exception) {
         println("${e.message}")
